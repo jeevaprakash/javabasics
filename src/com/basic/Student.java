3fee785b801c310d3129d1418a723751e6efc662
jeevaprakash <jeevaprakash694@gmail.com>
2018-05-29 18:33:56 +0530

    simple calculation

---
+++ src/com/basic/Student.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Student {
+  public static void main(String[] args)
+  {
+	 int x=0, total=0;
+	 while(x<=10)
+	 {
+		 total+=x;
+		 ++x;
+	 }
+	 System.out.println(total);
+	 
+  }
+}
