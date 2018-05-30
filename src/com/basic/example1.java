aa22eaa9dd58f30feb79cb489333fbf64671213f
jeevaprakash <jeevaprakash694@gmail.com>
2018-05-30 10:18:57 +0530

    example1 if else

---
+++ src/com/basic/example1.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class example1 
+{
+	public static void main(String[]args)
+	{
+	int age=32;
+  
+	if(age>30) {
+		System.out.println(age);
+	}
+	else 
+		System.out.println(age);
+	}		
+}
