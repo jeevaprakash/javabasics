cbd153d9d2d90703fad5ccc806706d1c4214da68
jeevaprakash <jeevaprakash694@gmail.com>
2018-05-30 11:28:10 +0530

    example5

---
+++ src/com/basic/example5.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class example5 {
+	public static void main(String[]args){
+	{
+		int i = 1;
+		while ( i <= 10 ) {			
+		++i;
+		System.out.println();
+		}
+		}
+
+	}
+
+}
