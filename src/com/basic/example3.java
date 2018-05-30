1fddfd8e2e9c1b038bf914aaeb90e4148dbbcea5
jeevaprakash <jeevaprakash694@gmail.com>
2018-05-30 11:25:37 +0530

    while example

---
+++ src/com/basic/example3.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class example3 {
+public static void main(String[]args) {
+	int a=20,b=40,c;
+	c=a+b;
+	while(c<50)
+	{
+		System.out.println("c is less than 50");
+	}
+	System.out.println("c is grater than 50");
+	
+}
+}
