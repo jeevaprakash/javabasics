90ccb46b70d0cf7d83c80ba67568f142b43c10cf
jeevaprakash <jeevaprakash694@gmail.com>
2018-06-01 18:32:24 +0530

    array

---
+++ src/com/basic/array.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class array {
+public static void main(String[] args) {
+	int s[]=new int[5];
+for (int i = 0; i < s.length; i++) {
+	s[i]=i*2;
+	
+}
+//int s[4]=50;
+int a[]= {10,23,45,67,78,88};
+for (int i = 0; i < args.length; i++) {
+	System.out.println(args[i]);
+}
+
+		}
+}
